17ac7369d13501c8b8e5a53a751e289e7ba44317
Yonatan Smolyar <ysmolyar@login-students.ccs.neu.edu>
2020-02-11 13:27:35 -0500

    data packets sending

--- utils.py
+++ utils.py
@@ -1,17 +1,21 @@
 def addr_in_range(addr, prefix, mask):
     """Determines if the given addr is in the range spanned by the given prefix and subnet mask"""
     addr_bin = addr_to_binary(addr)
-    cidr_bin = addr_to_binary(calculate_cidr(prefix, mask))
+    print("ADDR_BIN: " + str(addr_bin) + "\n")
+    cidr_bin = addr_to_binary(prefix)[:calculate_cidr_number(prefix, mask)]
+    print("CIDR_BIN: " + str(cidr_bin) + "\n")
     return addr_bin.startswith(cidr_bin)
 
 
-def calculate_cidr(addr, mask):
+def calculate_cidr_number(addr, mask):
     """Returns CIDR address from IPv4 address and subnet mask"""
-    return addr + '/' + str(addr_to_binary(mask).count('1'))
+    return addr_to_binary(mask).count('1')
 
 
 def addr_to_binary(addr):
     """Returns the binary string representation of the given IPv4 address, or prefix bits if CIDR address"""
-    binary = '{0:08b}{1:08b}{2:08b}{3:08b}'.format(*addr.split('.'))
-    subnet = int(addr.split('/')[1]) if '/' in addr else None
-    return binary[:subnet] if subnet else binary
+    bins = addr.split('.')
+    res = ""
+    for b in bins:
+        res += format(int(b), '08b')
+    return res
