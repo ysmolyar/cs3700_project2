b5a2bf24791d5dc0ed16351126e8074dc12d0e81
Yonatan Smolyar <ysmolyar@login-students.ccs.neu.edu>
2020-02-13 12:08:59 -0500

    passing level 2

--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
+def get_self_ip(addr):
+    """
+    Returns self IP given neighbor router IP by replacing last 8 bits with 1:
+        'For the sake of simplicity, all of the neighboring routers will have IP addresses of the
+        form *.*.*.2 and all of the IP addresses used by your router will be of the form *.*.*.1.'
+    """
+    return addr.rsplit('.', 1)[0] + '.1'
+
+
 def addr_in_range(addr, prefix, mask):
     """Determines if the given addr is in the range spanned by the given prefix and subnet mask"""
     addr_bin = addr_to_binary(addr)
