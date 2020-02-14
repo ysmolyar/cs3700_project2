928a90f971c7714d75f95e452741c3c450bc2d42
Yonatan Smolyar <ysmolyar@login-students.ccs.neu.edu>
2020-02-14 17:18:04 -0500

    passed level 6 tests

--- utils.py
+++ utils.py
@@ -10,19 +10,42 @@ def get_self_ip(addr):
 def addr_in_range(addr, prefix, mask):
     """Determines if the given addr is in the range spanned by the given prefix and subnet mask"""
     addr_bin = addr_to_binary(addr)
-    cidr_bin = addr_to_binary(prefix)[:calculate_cidr_number(prefix, mask)]
-    return addr_bin.startswith(cidr_bin)
+    subnet_bin = get_subnet(prefix, mask)
+    return addr_bin.startswith(subnet_bin) if subnet_bin != '' else False
 
 
-def calculate_cidr_number(addr, mask):
-    """Returns CIDR address from IPv4 address and subnet mask"""
+def get_subnet(addr, mask):
+    """Returns the binary string representation of the address subnet"""
+    return addr_to_binary(addr)[:get_cidr_number(mask)]
+
+
+def get_cidr_number(mask):
+    """Returns CIDR address from subnet mask"""
     return addr_to_binary(mask).count('1')
 
 
+def decrement_mask(mask):
+    """Decrements mask by one bit. Used for coalescing forwarding table entries"""
+    idx = get_cidr_number(mask) - 1
+    binary = addr_to_binary(mask)
+    dec = binary[:idx] + '0' + binary[idx + 1:]
+
+    return binary_to_addr(dec)
+
+
 def addr_to_binary(addr):
-    """Returns the binary string representation of the given IPv4 address, or prefix bits if CIDR address"""
+    """Returns the binary string representation of the given IPv4 address"""
     bins = addr.split('.')
     res = ""
     for b in bins:
         res += format(int(b), '08b')
     return res
+
+
+def binary_to_addr(bin):
+    """Converts binary string representation to decimal IPv4 address"""
+    words = (bin[0 + i:8 + i] for i in range(0, len(bin), 8))
+    res = ""
+    for w in words:
+        res += str(int(w, 2)) + '.'
+    return res[:-1]
